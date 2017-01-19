933d68a01e72625c2b89e949d4b6f474acc11e30
LegacyID1991 <contact@arvidwahlborg.com>
2017-01-19 12:23:42 +0100

    update back.bat

--- bin/back/back.bat
+++ bin/back/back.bat
@@ -1,12 +1,12 @@
 :: Terminal API
 ::
-:: This file is called whenever a user enters a shell.
+:: This file is called whenever a user wants to go up one step in the terminal
 ::
 :: Dependencies:
 ::   mb.bat add following = set PATH=%_GIT%\mindbender-core\bin\back\;%PATH%
 ::
 :: Example:
-::   $ mb
+::   $ back
 
 @echo off
 
@@ -16,11 +16,14 @@ IF "%PROJECTDIR%"=="" goto :return_to_mb
 IF "%ASSET%"=="" goto :return_to_mb
 
 IF NOT "%ASSET%"=="" goto :return_to_projects
+
 :return_to_mb
     call %_MB% M:\f01_projects %*
     SET PROJECTDIR=
+    endlocal
     exit /b
 :return_to_projects
     call _mkproject.bat %curPROJECT% %curPROJECTNAME% %PROJECTSTAGE%
     SET ASSET=
+    endlocal
     exit /b
