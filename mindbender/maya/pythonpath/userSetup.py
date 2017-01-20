0fd0aed397d2401750b7a1d0895227bc3c832ec1
LegacyID1991 <contact@arvidwahlborg.com>
2017-01-20 17:57:33 +0100

    revert back to old userSetup.py

---
+++ mindbender/maya/pythonpath/userSetup.py
@@ -0,0 +1,20 @@
+"""Maya initialisation for Mindbender pipeline"""
+
+from maya import cmds
+
+
+def setup():
+    assert __import__("pyblish_maya").is_setup(), (
+        "mindbender-core depends on pyblish_maya which has not "
+        "yet been setup. Run pyblish_maya.setup()")
+
+    from pyblish import api
+    api.register_gui("pyblish_lite")
+
+    from mindbender import api, maya
+    api.install(maya)
+
+
+# Allow time for dependencies (e.g. pyblish-maya)
+# to be installed first.
+cmds.evalDeferred(setup)
