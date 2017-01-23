d71eb61a7e89866721e5d30b8e0d74af22944753
LegacyID1991 <contact@arvidwahlborg.com>
2017-01-23 13:48:48 +0100

    add new part of Terminal API for asset category such as props and char under the assets folder

---
+++ bin/_mkassetscate.bat
@@ -0,0 +1,47 @@
+:: Assets Category API
+::
+:: This file is called whenever a user enters an asset, such as Cat or Shot01.
+::
+:: Arguments:
+::   %1: Name of asset, e.g. Bruce
+::   %2: Absolute path to asset parent directory, e.g. m:\f01_projects\p999_Demo\assets
+::
+:: Example:
+::   $ call _mkasset Bruce %~dp0
+
+@echo off
+
+If "%ASSETSCATE%"=="" goto :missing
+
+title %PROJECTDIR% / %ASSETSCATE%
+
+cd %ASSETSCATE%
+
+:: Clear screen
+cls
+
+echo+
+echo %PROJECTNAME%
+echo You are in: %ASSETSCATE% -------
+echo+
+
+:: List available assets, without their .bat suffix
+setlocal enabledelayedexpansion
+for %%i in (*.bat) do (
+    set temp=%%i
+    echo   !temp:.bat=!
+)
+endlocal
+
+
+echo+
+echo   1. Type first letters of asset category
+echo   2. Press [TAB] to auto-complete
+echo+
+echo  --------------------------------------
+
+goto :eof
+
+:missing
+    echo ERROR: Missing FULLPATH
+    exit /b
