f21cd7a9d2c84a6f1590680ecb6ad9cbcea17b8b
Marcus Ottosson <konstruktion@gmail.com>
2017-01-26 01:41:08 +0000

    Strengthen single assembly validator

--- mindbender/maya/plugins/validate_single_assembly.py
+++ mindbender/maya/plugins/validate_single_assembly.py
@@ -2,17 +2,34 @@ import pyblish.api
 
 
 class ValidateMindbenderSingleAssembly(pyblish.api.InstancePlugin):
-    """Each asset must have a single top-level group"""
+    """Each asset must have a single top-level group
 
-    label = "Validate Single assembly"
+    The given instance is test-exported, along with construction
+    history to test whether more than 1 top-level DAG node would
+    be included in the exported file.
+
+    """
+
+    label = "Validate Single Assembly"
     order = pyblish.api.ValidatorOrder
     hosts = ["maya"]
     families = ["mindbender.model", "mindbender.rig"]
 
     def process(self, instance):
         from maya import cmds
+        from mindbender import maya
 
-        assemblies = cmds.ls(instance, assemblies=True)
+        with maya.maintained_selection():
+            cmds.select(instance, replace=True)
+            nodes = cmds.file(
+                constructionHistory=True,
+                exportSelected=True,
+                preview=True,
+                force=True,
+            )
+
+        assemblies = cmds.ls(nodes, assemblies=True)
+
         assert len(assemblies) == 1, (
             ("Multiple assemblies found."
                 if len(assemblies) > 1
