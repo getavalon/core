9ebf608a88f87f5430fe71172d9a0cb268b9ac03
LegacyID1991 <contact@arvidwahlborg.com>
2017-02-01 15:30:12 +0100

    Update with documentation comments and new globalvariables default values.

--- template/AssetName.bat
+++ template/AssetName.bat
@@ -1,2 +1,13 @@
+:: :: Following is the commands that needs to be called in the bat
+:: :: always call _mkproject
+:: %~n0 is the name of this project as the name of the .bat
+:: %~dp0 is drive plus path variable
 @echo off
 call _mkasset %~n0 %~dp0%
+
+:: :: Following is the variables that can be set in the bat
+:: :: They are default set to "nothing"
+:: :: leave them be if you dont have the info for them and return to them later.
+set OBJECT3D=
+Set MINDBENDER_EDIT_IN=
+Set MINDBENDER_EDIT_OUT=
--- template/ProjectName.bat
+++ template/ProjectName.bat
@@ -1,6 +1,17 @@
+:: :: Following is the commands that needs to be called in the bat
+:: :: always call _mkproject
 :: %~dp0 is drive plus path variable
 :: %~n0 is the name of this project as the name of the .bat
+:: \\f02_prod is the folder that you will be
 :: %1 is the entry by the user for asset
-
 @echo off
-call _mkproject %~dp0 %~n0 %1
+call _mkproject %~dp0 %~n0 \\f02_prod %1
+
+:: :: Following is the variables that can be set in the bat
+:: :: They are default set to "nothing"
+:: :: leave them be if you dont have the info for them and return to them later.
+set MINDBENDER_SETASSETCATEGORY=
+set MINDBENDER_ASSETCATEGORY=
+set MINDBENDER_FPS=
+set MINDBENDER_RESOLUTION_WIDTH=
+set MINDBENDER_RESOLUTION_HEIGHT=
