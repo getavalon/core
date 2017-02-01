25028f742b625ff921b51412d4abe1ae7bf09299
LegacyID1991 <contact@arvidwahlborg.com>
2017-02-01 10:13:05 +0100

    update userSetup     add fps set     add format of width and height variable     add time set of animation

--- mindbender/maya/pythonpath/userSetup.py
+++ mindbender/maya/pythonpath/userSetup.py
@@ -2,6 +2,16 @@
 
 from maya import cmds
 
+import os
+import mayafpsconverter
+
+
+MINDBENDER_FPS = mayafpsconverter(os.getenv("MINDBENDER_FPS"))
+MINDBENDER_EDIT_IN = os.getenv("MINDBENDER_EDIT_IN")
+MINDBENDER_EDIT_OUT = os.getenv("MINDBENDER_EDIT_OUT")
+MINDBENDER_RESOLUTION_WIDTH = os.getenv("MINDBENDER_RESOLUTION_WIDTH")
+MINDBENDER_RESOLUTION_HEIGHT = os.getenv("MINDBENDER_RESOLUTION_HEIGHT")
+
 
 def setup():
     assert __import__("pyblish_maya").is_setup(), (
@@ -18,3 +28,9 @@ def setup():
 # Allow time for dependencies (e.g. pyblish-maya)
 # to be installed first.
 cmds.evalDeferred(setup)
+if not MINDBENDER_FPS == "":
+    cmds.currentUnit(time=MINDBENDER_FPS)
+if not MINDBENDER_EDIT_IN == "":
+    cmds.playbackOption(animationEndTime=MINDBENDER_EDIT_IN)
+if not MINDBENDER_EDIT_OUT == "":
+    cmds.playbackOption(animationEndTime=MINDBENDER_EDIT_OUT)
