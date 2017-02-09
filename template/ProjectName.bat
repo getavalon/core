3bc788a73502feef4438f078ba65221016d51277
LegacyID1991 <contact@arvidwahlborg.com>
2017-02-09 15:46:31 +0100

    add more info for user about the new variables added before.

--- template/ProjectName.bat
+++ template/ProjectName.bat
@@ -1,15 +1,18 @@
-:: :: Following is the commands that needs to be called in the bat
-:: :: always call _mkproject
+:: Following is the commands that needs to be called in the bat
+:: always call _mkproject
 :: %~dp0 is drive plus path variable
 :: %~n0 is the name of this project as the name of the .bat
-:: \\f02_prod is the folder that you will be
 :: %1 is the entry by the user for asset
 @echo off
 call _mkproject %~dp0 %~n0 %1
 
-:: :: Following is the variables that can be set in the bat
-:: :: They are default set to "nothing"
-:: :: leave them be if you dont have the info for them and return to them later.
+:: Following is the variables that can be set in the bat
+:: They are default set to "nothing"
+:: leave them be if you dont have the info for them and return to them later.
+:: MINDBENDER_SETASSETCATEGORY should always be set to nothing, this is a reset
+:: MINDBENDER_ASSETCATEGORY expects categorys split by a "," exampel "prop, env, char"
+:: MINDBENDER_FPS, MINDBENDER_RESOLUTION_WIDTH, MINDBENDER_RESOLUTION_HEIGHT expects a 
+:: number value without letter characters
 set MINDBENDER_SETASSETCATEGORY=
 set MINDBENDER_ASSETCATEGORY=
 set MINDBENDER_FPS=
