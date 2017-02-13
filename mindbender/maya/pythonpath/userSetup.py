facb71ecf919f452d55d617bb7ffcde321292233
Marcus Ottosson <konstruktion@gmail.com>
2017-02-13 11:26:59 +0000

    Refactored userSetup.py The userSetup can never fail, especially not interactively; it handles initialisation and there is no telling what may go wrong if at any point in the module it stops running.
    Also there were no possible values except the ones left here. Anything else is a RuntimeError, so I managed to trim the size. Important to keep in mind that the userSetup must remain highly readable and straightforward, due to the above requirement. Anything taking up too many lines is best left to a dedicated module.

--- mindbender/maya/pythonpath/userSetup.py
+++ mindbender/maya/pythonpath/userSetup.py
@@ -5,33 +5,6 @@ from maya import cmds
 import os
 
 
-def mayafpsconverter(fps):
-    ERRORSTRING = "MINDBENDER_FPS variable has letters or bad value check the bat file"
-    """This checks to see that the value in MINDBENDER_FPS.string
-    doesn't conttain any letters
-    exampel "12" is valid
-    exampel "1a2" is invalid
-    """
-    if str(fps).isdigit() is False:
-        cmds.confirmDialog(title="Enviroment variable error",
-                           message=ERRORSTRING,
-                           button="",
-                           defaultButton="",
-                           cancelButton="",
-                           dismissString="")
-        return ""
-    return {
-        "15": "game",
-        "24": "film",
-        "25": "pal",
-        "30": "ntsc",
-        "48": "show",
-        "50": "palf",
-        "60": "ntscf",
-        None: "",
-        "": "", }.get(fps, fps + "fps")
-
-
 def setup():
     assert __import__("pyblish_maya").is_setup(), (
         "mindbender-core depends on pyblish_maya which has not "
@@ -43,18 +16,26 @@ def setup():
     from mindbender import api, maya
     api.install(maya)
 
-    MINDBENDER_FPS = os.getenv("MINDBENDER_FPS")
-    MINDBENDER_EDIT_IN = os.getenv("MINDBENDER_EDIT_IN")
-    MINDBENDER_EDIT_OUT = os.getenv("MINDBENDER_EDIT_OUT")
-    MINDBENDER_RESOLUTION_WIDTH = os.getenv("MINDBENDER_RESOLUTION_WIDTH")
-    MINDBENDER_RESOLUTION_HEIGHT = os.getenv("MINDBENDER_RESOLUTION_HEIGHT")
+    FPS = {
+        "15": "game",
+        "24": "film",
+        "25": "pal",
+        "30": "ntsc",
+        "48": "show",
+        "50": "palf",
+        "60": "ntscf"
+    }.get(os.getenv("MINDBENDER_FPS"), "pal")  # Default to "pal"
 
-    if MINDBENDER_FPS is not None:
-        cmds.currentUnit(time=(mayafpsconverter(MINDBENDER_FPS)))
-    if MINDBENDER_EDIT_IN is not None:
-        cmds.playbackOptions(animationStartTime=MINDBENDER_EDIT_IN)
-    if MINDBENDER_EDIT_OUT is not None:
-        cmds.playbackOptions(animationEndTime=MINDBENDER_EDIT_OUT)
+    EDIT_IN = os.getenv("MINDBENDER_EDIT_IN") or 1000
+    EDIT_OUT = os.getenv("MINDBENDER_EDIT_OUT") or 1200
+    RESOLUTION_WIDTH = os.getenv("MINDBENDER_RESOLUTION_WIDTH") or 1920
+    RESOLUTION_HEIGHT = os.getenv("MINDBENDER_RESOLUTION_HEIGHT") or 1080
+
+    cmds.setAttr("defaultResolution.width", RESOLUTION_WIDTH)
+    cmds.setAttr("defaultResolution.height", RESOLUTION_HEIGHT)
+    cmds.currentUnit(time=FPS)
+    cmds.playbackOptions(animationStartTime=EDIT_IN)
+    cmds.playbackOptions(animationEndTime=EDIT_OUT)
 
 
 # Allow time for dependencies (e.g. pyblish-maya)
