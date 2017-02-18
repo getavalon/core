c7e8e16394c4ee84fc6f2392b5c59edaa36f6e91
Marcus Ottosson <konstruktion@gmail.com>
2017-02-18 09:28:49 +0000

    Fix #46

--- mindbender/maya/pythonpath/userSetup.py
+++ mindbender/maya/pythonpath/userSetup.py
@@ -26,16 +26,24 @@ def setup():
         "60": "ntscf"
     }.get(os.getenv("MINDBENDER_FPS"), "pal")  # Default to "pal"
 
-    EDIT_IN = os.getenv("MINDBENDER_EDIT_IN") or 101
-    EDIT_OUT = os.getenv("MINDBENDER_EDIT_OUT") or 201
-    RESOLUTION_WIDTH = os.getenv("MINDBENDER_RESOLUTION_WIDTH") or 1920
-    RESOLUTION_HEIGHT = os.getenv("MINDBENDER_RESOLUTION_HEIGHT") or 1080
+    EDIT_IN = getenv("MINDBENDER_EDIT_IN", int) or 101
+    EDIT_OUT = getenv("MINDBENDER_EDIT_OUT", int) or 201
+    RESOLUTION_WIDTH = getenv("MINDBENDER_RESOLUTION_WIDTH", int) or 1920
+    RESOLUTION_HEIGHT = getenv("MINDBENDER_RESOLUTION_HEIGHT", int) or 1080
 
     cmds.setAttr("defaultResolution.width", RESOLUTION_WIDTH)
     cmds.setAttr("defaultResolution.height", RESOLUTION_HEIGHT)
     cmds.currentUnit(time=FPS)
     cmds.playbackOptions(animationStartTime=EDIT_IN)
     cmds.playbackOptions(animationEndTime=EDIT_OUT)
+
+
+def getenv(var, typ):
+    """Return `var` from environment as `typ`"""
+    try:
+        return typ(os.getenv(var))
+    except TypeError:
+        return None
 
 
 # Allow time for dependencies (e.g. pyblish-maya)
