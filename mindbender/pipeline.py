9e2fa73287bfc10a06cd70c039766faf53d774b2
Marcus Ottosson <konstruktion@gmail.com>
2017-04-26 14:37:45 +0100

    Default root to MINDBENDER_PROJECTPATH

--- mindbender/pipeline.py
+++ mindbender/pipeline.py
@@ -484,7 +484,7 @@ def registered_root():
     """Return currently registered root"""
     return (
         _registered_root["_"] or
-        os.getenv("MINDBENDER_ROOT") or ""
+        os.getenv("MINDBENDER_PROJECTPATH") or ""
     ).replace("\\", "/")
 
 
