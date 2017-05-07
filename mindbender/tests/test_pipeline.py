6ee49df870aed711184fbf122004174f0a1f338d
Marcus Ottosson <konstruktion@gmail.com>
2017-05-07 15:31:41 +0100

    Update tests

--- mindbender/tests/test_pipeline.py
+++ mindbender/tests/test_pipeline.py
@@ -6,7 +6,6 @@
 
 import os
 import sys
-import json
 import types
 import shutil
 import tempfile
@@ -31,6 +30,7 @@ def clear():
 def setup():
     self.tempdir = tempfile.mkdtemp()
     pipeline.register_root(self.tempdir)
+    os.environ["MINDBENDER_PROJECTPATH"] = self.tempdir
 
     # Mock host
     host = types.ModuleType("Test")
@@ -47,150 +47,6 @@ def setup():
 
 def teardown():
     shutil.rmtree(self.tempdir)
-
-
-@contextlib.contextmanager
-def bad_fixture():
-    empty = os.path.join(self.tempdir, "empty")
-    os.makedirs(empty)
-
-    try:
-        yield empty
-    finally:
-        shutil.rmtree(empty)
-
-
-def test_ls():
-    """ls() returns available assets from current root directory
-
-    ls() returns a formatted list of available assets. For an asset
-    to be recognised as an asset, it must adhere to a strict schema.
-
-     ________________________________ ________________________________
-    |          |          |          |          |          |          |
-    | version1 | version2 | version3 | version1 | version2 | version3 |
-    |__________|__________|__________|__________|__________|__________|
-    |                                |                                |
-    |            subset1             |             subset1            |
-    |________________________________|________________________________|
-    |                                |                                |
-    |            asset1              |             asset2             |
-    |________________________________|________________________________|
-    |                                                                 |
-    |                             project                             |
-    |_________________________________________________________________|
-
-    This schema is located within each version of an asset and is
-    denoted `mindbender-core:version-1.0`.
-
-    The members of this schema is also strict, they are:
-
-    {
-        "schema": "mindbender-core:version-1.0",
-        "name": "Name of asset",
-        "representations": [List of representations],
-    }
-
-    Where each `representation` follows the following schema.
-
-    {
-        "format": "file extension or similar identifier",
-        "source": "absolute path to source file",
-        "author": "original author of version",
-        "path": "relative path to file, starting at {root}"
-    }
-
-    The returned dictionary is also strictly defined.
-
-    """
-
-    parent = "assets"
-
-    with pipeline.fixture(assets=["Asset1"],
-                          subsets=["modelDefault"],
-                          versions=1) as root:
-        asset = next(pipeline.ls([parent]))
-
-    reference = {
-        "schema": "mindbender-core:asset-1.0",
-        "name": "Asset1",
-        "subsets": [
-            {
-                "schema": "mindbender-core:subset-1.0",
-                "name": "modelDefault",
-                "versions": [
-                    {
-                        "schema": "mindbender-core:version-1.0",
-                        "families": ["mindbender.model"],
-                        "version": 1,
-                        "path": os.path.join(
-                            root,
-                            parent,
-                            "Asset1",
-                            "publish",
-                            "modelDefault",
-                            "v001"
-                        ),
-                        "source": os.path.join(
-                            "{project}",
-                            "maya",
-                            "scenes",
-                            "scene.ma"
-                        ),
-                        "representations": [
-                            {
-                                "schema": ("mindbender-core:"
-                                           "representation-1.0"),
-                                "format": ".ma",
-                                "path": os.path.join(
-                                    "{dirname}",
-                                    "Asset1{format}"
-                                ),
-                            }
-                        ],
-                        "time": "",
-                        "author": "mottosso",
-                    },
-                ]
-            }
-        ]
-    }
-
-    # Printed on error
-    print("# Comparing result:")
-    print(json.dumps(asset, indent=4, sort_keys=True))
-    print("# With reference:")
-    print(json.dumps(reference, indent=4, sort_keys=True))
-
-    assert_equals(asset, reference)
-
-
-def test_ls_returns_sorted_versions():
-    """Versions returned from ls() are alphanumerically sorted"""
-    with pipeline.fixture(assets=["Asset1"],
-                          subsets=["modelDefault"],
-                          versions=1):
-        for asset in pipeline.ls(["assets"]):
-            previous_version = 0
-            for subset in asset["subsets"]:
-                for version in subset["versions"]:
-                    version = version["version"]
-                    assert version > previous_version
-                    previous_version = version
-
-
-def test_ls_empty():
-    """No assets results in an empty generator"""
-    with pipeline.fixture(assets=[], versions=0):
-        assert_raises(StopIteration, next, pipeline.ls(["assets"]))
-
-
-def test_ls_no_shareddir():
-    """A root without assets returns an empty generator"""
-
-    with bad_fixture() as root:
-        pipeline.register_root(root)
-        assert next(pipeline.ls(), None) is None
 
 
 @with_setup(clear)
