8c026ca9207296285e3803a52f6eba4daf899ded
Marcus Ottosson <konstruktion@gmail.com>
2017-05-07 15:28:26 +0100

    Update integration plug-in

--- mindbender/plugins/integrate_asset.py
+++ mindbender/plugins/integrate_asset.py
@@ -2,7 +2,7 @@ import pyblish.api
 
 
 class IntegrateMindbenderAsset(pyblish.api.InstancePlugin):
-    """Move user data to shared location
+    """Write to files and metadata
 
     This plug-in exposes your data to others by encapsulating it
     into a new version.
@@ -35,19 +35,15 @@ class IntegrateMindbenderAsset(pyblish.api.InstancePlugin):
 
     def process(self, instance):
         import os
-        import json
         import errno
         import shutil
-        from mindbender import api
+        from pprint import pformat
 
-        assert os.getenv("MINDBENDER_ASSETPATH"), (
-            "Missing environment variable MINDBENDER_ASSETPATH\n"
-            "This can sometimes happen when an application was launched \n"
-            "manually, outside of the pipeline."
-        )
+        from mindbender import api, io
 
-        assert os.getenv("MINDBENDER_SILO"), (
-            "Missing environment variable MINDBENDER_SILO\n"
+        assert all(os.getenv(env) for env in (
+            "MINDBENDER__ASSET", "MINDBENDER__PROJECT")), (
+            "Missing environment variables\n"
             "This can sometimes happen when an application was launched \n"
             "manually, outside of the pipeline."
         )
@@ -85,20 +81,157 @@ class IntegrateMindbenderAsset(pyblish.api.InstancePlugin):
 
         self.log.debug("Establishing staging directory @ %s" % stagingdir)
 
-        root = os.getenv("MINDBENDER_ASSETPATH")
-        instancedir = os.path.join(root, "publish", instance.data["subset"])
+        project = io.find_one({
+            "_id": io.ObjectId(os.environ["MINDBENDER__PROJECT"])
+        })
 
-        try:
-            os.makedirs(instancedir)
-        except OSError as e:
-            if e.errno != errno.EEXIST:  # Already exists
-                self.log.critical("An unexpected error occurred.")
-                raise
+        asset = io.find_one({
+            "_id": io.ObjectId(os.environ["MINDBENDER__ASSET"])
+        })
 
-        version = api.find_latest_version(os.listdir(instancedir)) + 1
-        versiondir = os.path.join(instancedir, api.format_version(version))
+        assert all([project, asset])
 
-        self.log.debug("New version: %i" % version)
+        subset = io.find_one({
+            "type": "subset",
+            "parent": asset["_id"],
+            "name": instance.data["subset"]
+        })
+
+        if subset is None:
+            self.log.info("Subset '%s' not found, creating.."
+                          % instance.data["subset"])
+
+            _id = io.insert_one({
+                "schema": "mindbender-core:subset-2.0",
+                "type": "subset",
+                "name": instance.data["subset"],
+                "parent": asset["_id"]
+            }).inserted_id
+
+            subset = io.find_one({"_id": _id})
+
+        all_versions = [0] + [
+            version["name"]
+            for version in io.find({
+                "type": "version",
+                "parent": subset["_id"]
+            }, {"name": True})
+        ]
+
+        next_version = sorted(all_versions)[-1] + 1
+
+        # versiondir = template_versions.format(**template_data)
+        self.log.debug("Next version: %i" % next_version)
+
+        version = {
+            "schema": "mindbender-core:version-2.0",
+            "type": "version",
+            "parent": subset["_id"],
+            "name": next_version,
+
+            # Used to identify family of assets already on disk
+            "families": instance.data.get("families", list()) + [
+                instance.data.get("family")
+            ],
+
+            "time": context.data["time"],
+            "author": context.data["user"],
+
+            "source": os.path.join(
+                "{root}",
+                os.path.relpath(
+                    context.data["currentFile"],
+                    api.registered_root()
+                )
+            ).replace("\\", "/"),
+        }
+
+        self.backwards_compatiblity(instance, version)
+
+        self.log.debug("Creating version: %s" % pformat(version))
+        version_id = io.insert_one(version).inserted_id
+
+        # Write to disk
+        #          _
+        #         | |
+        #        _| |_
+        #    ____\   /
+        #   |\    \ / \
+        #   \ \    v   \
+        #    \ \________.
+        #     \|________|
+        #
+        template_data = {
+            "root": api.registered_root(),
+            "project": os.environ["MINDBENDER_PROJECT"],
+            "silo": os.environ["MINDBENDER_SILO"],
+            "asset": os.environ["MINDBENDER_ASSET"],
+            "subset": subset["name"],
+            "version": version["name"],
+        }
+
+        template_publish = project["template"]["publish"]
+
+        for fname in os.listdir(stagingdir):
+            name, ext = os.path.splitext(fname)
+            template_data["representation"] = ext[1:]
+
+            src = os.path.join(stagingdir, fname)
+            dst = template_publish.format(**template_data)
+
+            # Backwards compatibility
+            if ext == ".json":
+                dirname = os.path.dirname(dst)
+                dst = os.path.join(dirname, ".metadata.json")
+
+            self.log.info("Copying %s -> %s" % (src, dst))
+
+            dirname = os.path.dirname(dst)
+            try:
+                os.makedirs(dirname)
+            except OSError as e:
+                if e.errno == errno.EEXIST:
+                    pass
+                else:
+                    self.log.critical("An unexpected error occurred.")
+                    raise
+
+            shutil.copy(src, dst)
+
+            representation = {
+                "schema": "mindbender-core:representation-2.0",
+                "type": "representation",
+                "parent": version_id,
+                "name": ext[1:],
+                "label": {
+                    ".ma": "Maya Ascii",
+                    ".source": "Original source file",
+                    ".abc": "Alembic"
+                }.get(ext)
+            }
+
+            io.insert_one(representation)
+
+        self.log.info("Successfully integrated \"%s\" to \"%s\"" % (
+            instance, dst))
+
+    def backwards_compatiblity(self, instance, version):
+        """Maintain backwards compatibility with newly published assets
+
+        With the introduction of the database in 2.0, the artist would be
+        unable to publish in 2.0 and use the files in 1.0. Therefore, we
+        introduce this mechanism which continue to write for 1.0 even
+        when writing from the 2.0 pipeline.
+
+        This behaviour is deprecated and is to be removed in a future release.
+
+        """
+
+        import os
+        import json
+        from mindbender import api
+
+        context = instance.context
 
         # Metadata
         #  _________
@@ -109,24 +242,51 @@ class IntegrateMindbenderAsset(pyblish.api.InstancePlugin):
         # |         |
         # |_________|
         #
+        stagingdir = instance.data.get("stagingDir")
         fname = os.path.join(stagingdir, ".metadata.json")
+
+        root = os.getenv("MINDBENDER_ASSETPATH")
+        instancedir = os.path.join(root, "publish", instance.data["subset"])
+
+        latest_version = api.find_latest_version(os.listdir(instancedir)) + 1
+        versiondir = os.path.join(
+            instancedir,
+            api.format_version(latest_version)
+        )
 
         try:
             with open(fname) as f:
-                metadata = json.load(f)
+                version_1_0 = json.load(f)
 
         except IOError:
-            metadata = {
+            d = os.path.relpath(
+                context.data["currentFile"],
+                os.path.join(
+                    api.registered_root(),
+                    os.environ["MINDBENDER_PROJECT"]
+                )
+            )
+            self.log.info(d)
+            version_1_0 = dict(version, **{
                 "schema": "mindbender-core:version-1.0",
-                "version": version,
                 "path": os.path.join(
                     "{root}",
                     os.path.relpath(
                         versiondir,
-                        api.registered_root()
+                        os.path.join(
+                            api.registered_root(),
+
+                            # The meaning of "root" was changed
+                            # in 2.0 to mean the root of all projects.
+                            # Before, it was the root of one project,
+                            # so we compensate by mimicking this behaviour.
+                            os.environ["MINDBENDER_PROJECT"]
+                        )
                     )
                 ).replace("\\", "/"),
                 "representations": list(),
+
+                "version": version["name"],
 
                 # Used to identify family of assets already on disk
                 "families": instance.data.get("families", list()) + [
@@ -144,17 +304,23 @@ class IntegrateMindbenderAsset(pyblish.api.InstancePlugin):
                     "{root}",
                     os.path.relpath(
                         context.data["currentFile"],
-                        api.registered_root()
+                        os.path.join(
+                            api.registered_root(),
+                            os.environ["MINDBENDER_PROJECT"]
+                        )
                     )
                 ).replace("\\", "/"),
-            }
+
+                # Discard database keys
+                "parent": None,
+            })
 
         self.log.debug("Finished generating metadata: %s"
-                       % json.dumps(metadata, indent=4))
+                       % json.dumps(version_1_0, indent=4))
 
         for filename in instance.data.get("files", list()):
             name, ext = os.path.splitext(filename)
-            metadata["representations"].append(
+            version_1_0["representations"].append(
                 {
                     "schema": "mindbender-core:representation-1.0",
                     "format": ext,
@@ -176,13 +342,6 @@ class IntegrateMindbenderAsset(pyblish.api.InstancePlugin):
         #     \|________|
         #
         with open(fname, "w") as f:
-            json.dump(metadata, f, indent=4)
+            json.dump(version_1_0, f, indent=4)
 
-        # Metadata is written before being validated -
-        # this way, if validation fails, the data can be
-        # inspected by hand from within the user directory.
-        api.schema.validate(metadata, "version")
-        shutil.copytree(stagingdir, versiondir)
-
-        self.log.info("Successfully integrated \"%s\" to \"%s\"" % (
-            instance, versiondir))
+        self.log.info("Successfully wrote %s." % fname)
