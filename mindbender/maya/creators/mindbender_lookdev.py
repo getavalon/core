a7dc50018c96e813bd21ab80a44e813379961d9d
Marcus Ottosson <konstruktion@gmail.com>
2017-06-15 09:54:01 +0100

    Move plug-ins into external configuration

--- mindbender/maya/creators/mindbender_lookdev.py
+++
@@ -1,9 +0,0 @@
-from mindbender import maya
-
-
-class CreateLookdev(maya.Creator):
-    """Shaders, textures and look"""
-
-    name = "lookdevDefault"
-    label = "Lookdev"
-    family = "mindbender.lookdev"
