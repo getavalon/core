3b6768fa9c43b3010105a1ff7e5346955993b2c7
Marcus Ottosson <konstruktion@gmail.com>
2017-08-01 19:34:39 +0100

    Re-implement Make Available Offline

--- avalon/tools/loader/app.py
+++ avalon/tools/loader/app.py
@@ -1,12 +1,26 @@
 import os
 import sys
 import time
+import errno
+import shutil
+import logging
+import tempfile
 import datetime
+import threading
 
+try:
+    import Queue as queue
+except ImportError:
+    # Python 3+
+    import queue
+
+from ...vendor import requests
 from ...vendor.Qt import QtWidgets, QtCore, QtGui
 from ... import api, io
 from .. import lib
 from ...vendor import qtawesome as awesome
+
+log = logging.getLogger(__name__)
 
 module = sys.modules[__name__]
 module.window = None
@@ -21,14 +35,80 @@ LatestRole = QtCore.Qt.UserRole + 3
 LocationRole = QtCore.Qt.UserRole + 4
 
 
+# About to be downloaded
+module._downloads = queue.Queue()
+
+# Which downloads are in progress?
+module._downloading = dict()
+
+
+def exists(src):
+    resp = requests.head(src)
+    print resp.status_code, resp.text, resp.headers
+
+
+def download(src, dst):
+    basename = os.path.basename(src)
+    tmp = os.path.join(tempfile.gettempdir(), basename)
+
+    try:
+        response = requests.get(
+            src,
+            stream=True,
+            auth=requests.auth.HTTPBasicAuth(
+                api.Session["AVALON_USERNAME"],
+                api.Session["AVALON_PASSWORD"]
+            )
+        )
+    except requests.ConnectionError as e:
+        yield None, e
+        return
+
+    with open(tmp, "wb") as f:
+
+        total_length = response.headers.get("content-length")
+
+        if total_length is None:  # no content length header
+            f.write(response.content)
+        else:
+            downloaded = 0
+            total_length = int(total_length)
+            for data in response.iter_content(chunk_size=4096):
+                downloaded += len(data)
+                f.write(data)
+
+                if module.debug:
+                    time.sleep(0.007)
+
+                yield int(100.0 * downloaded / total_length), None
+
+    try:
+        os.makedirs(os.path.dirname(dst))
+    except OSError as e:
+        # An already existing working directory is fine.
+        if e.errno != errno.EEXIST:
+            raise
+
+    shutil.copy(tmp, dst)
+
+    # Clean up
+    os.remove(tmp)
+
+
 class Window(QtWidgets.QDialog):
     """Asset loader interface"""
+
+    download_progressed = QtCore.Signal(str, int)
+    download_completed = QtCore.Signal()
+    download_errored = QtCore.Signal(str)
 
     def __init__(self, parent=None):
         super(Window, self).__init__(parent)
         self.setWindowTitle(
-            "Asset Loader 2.1 - %s/%s" % (
-                module.root, module.project))
+            "Asset Loader 2.1 - {root}/{project}".format(
+                root=api.Session["AVALON_PROJECTS"],
+                project=api.Session["AVALON_PROJECT"])
+        )
 
         self.setFocusPolicy(QtCore.Qt.StrongFocus)
 
@@ -75,6 +155,67 @@ QPushButton {
     font-family: "FontAwesome";
 }
 """)
+
+        offline_slider = QtWidgets.QSlider(QtCore.Qt.Horizontal)
+        offline_slider.setRange(0, 1)
+        offline_slider.setFixedHeight(25)
+        offline_slider.setStyleSheet("""
+QSlider {
+    max-width: 100px;
+}
+
+QSlider::groove:horizontal {
+    border: 1px solid #666;
+    height: 23px;
+    background: #777;
+    margin: 2px 0;
+    border-radius: 4px;
+}
+
+QSlider::groove:horizontal:enabled {
+    border: 1px solid #222;
+    height: 23px;
+    background: qlineargradient(x1:0, y1:0, \
+                                x2:0, y2:1, \
+                                stop:0 #555, \
+                                stop:1 #666);
+    margin: 2px 0;
+    border-radius: 4px;
+}
+
+QSlider::handle:horizontal {
+    background: #777;
+    border: 1px solid #555;
+    width: 45px;
+    margin: 1px;
+    border-radius: 2px;
+}
+
+QSlider::handle:horizontal:enabled {
+    border: 1px solid #333;
+    background: qlineargradient(x1:0, y1:0, \
+                                x2:1, y2:1, \
+                                stop:0 #aaa \
+                                stop:1 #888);
+}
+
+""")
+        offline_label = QtWidgets.QLabel("Available offline..")
+        offline_on = QtWidgets.QLabel("On", offline_slider)
+        offline_off = QtWidgets.QLabel("Off", offline_slider)
+        offline_on.move(72, 6)
+        offline_off.move(16, 6)
+
+        for toggle in (offline_on, offline_off):
+            toggle.show()
+            toggle.setStyleSheet("QLabel { color: black; }")
+
+        offline = QtWidgets.QWidget()
+
+        layout = QtWidgets.QHBoxLayout(offline)
+        layout.addWidget(offline_label)
+        layout.addWidget(offline_slider)
+        layout.setContentsMargins(0, 0, 0, 0)
 
         message = QtWidgets.QLabel()
         message.hide()
@@ -145,6 +286,7 @@ QPushButton {
         layout.addWidget(side_preset_container)
         layout.addWidget(QtWidgets.QWidget(), 1)
         layout.addWidget(options, 0, QtCore.Qt.AlignBottom)
+        layout.addWidget(offline)
         layout.addWidget(buttons)
         layout.setSpacing(10)
         layout.setContentsMargins(0, 0, 0, 0)
@@ -161,6 +303,7 @@ QPushButton {
             "button": {
                 "load": load_button,
                 "autoclose": autoclose_checkbox,
+                "offline": offline_slider,
             },
             "model": {
                 "assets": assets,
@@ -181,7 +324,6 @@ QPushButton {
             },
             "state": {
                 "template": None,
-                "locations": list(),
                 "context": {
                     "root": None,
                     "project": None,
@@ -194,6 +336,11 @@ QPushButton {
             }
         }
 
+        self.download_progressed.connect(self.on_download_progressed)
+        self.download_completed.connect(self.on_download_completed)
+        self.download_errored.connect(self.on_download_errored)
+
+        offline_slider.valueChanged.connect(self.on_make_available_offline)
         load_button.clicked.connect(self.on_load_pressed)
         refresh_button.clicked.connect(self.on_refresh_pressed)
         assets.currentItemChanged.connect(self.on_assetschanged)
@@ -206,6 +353,41 @@ QPushButton {
         self.resize(1100, 600)
 
         load_button.setEnabled(False)
+        offline_slider.setEnabled(False)
+
+        thread = threading.Thread(target=self._download_manager)
+        thread.daemon = True
+        thread.start()
+
+    def _download_manager(self):
+        while True:
+            log.info("Listening for downloads..")
+            src, dst = module._downloads.get()
+
+            # Killswitch
+            if not any([src, dst]):
+                break
+
+            previous = 0
+            for progress, error in download(src, dst):
+
+                if error:
+                    self.download_errored.emit(
+                        "ERROR: Could not download %s" % src)
+                    break
+
+                # Avoid emitting signals needlessly.
+                # Progress is sometimes less than one whole percent.
+                if progress != previous:
+                    previous = progress
+                    self.download_progressed.emit(src, progress)
+
+            module._downloading.pop(dst)
+
+            if not error:
+                self.download_completed.emit()
+
+            log.info("Finished with my current download")
 
     def on_copy_source_to_clipboard(self):
         source = self.data["label"]["source"].text()
@@ -220,6 +402,79 @@ QPushButton {
         action.triggered.connect(self.on_copy_source_to_clipboard)
         menu.move(pos)
         menu.exec_()
+
+    def on_download_progressed(self, fname, progress):
+        self.echo("Downloading ({remaining}) {fname}.. {progress}%".format(
+            remaining=module._downloads.qsize() + 1,
+            fname=fname,
+            progress=progress
+        ))
+
+    def on_download_completed(self):
+        self._representationschanged()
+        self.echo("Done")
+
+    def on_download_errored(self, message):
+        self._representationschanged()
+        self.echo(message)
+
+    def on_make_available_offline(self, state):
+        if state != 1:
+            return
+
+        if not self.data["button"]["offline"].isEnabled():
+            return
+
+        offline = self.data["button"]["offline"]
+        models = self.data["model"]
+
+        representations_model = models["representations"]
+        representation_item = representations_model.currentItem()
+
+        for representation in representation_item.data(RepresentationsRole):
+            version = io.find_one({"_id": representation["parent"]})
+
+            # Backwards compatibility, older members did not have this key
+            locations = version.get("locations", list())
+
+            if not locations:
+                # Support global default, primarily intended during
+                # the transition from not using locations.
+                try:
+                    locations = [api.Session["AVALON_LOCATION"]]
+                except KeyError:
+                    locations = list()
+
+            try:
+                # TODO(marcus): Support multiple locations
+                location = locations[0]
+            except IndexError:
+                offline.setToolTip("Couldn't find where to "
+                                   "download this from..")
+                continue
+
+            try:
+                template = self.data["state"]["template"]
+                context = self.data["state"]["context"].copy()
+                context["root"] = location + "/download"
+                path = template.format(**context)
+
+            except Exception as e:
+                log.error(e)
+                self.echo("Something went wrong..")
+
+            else:
+                context = self.data["state"]["context"]
+
+                src = path
+                dst = template.format(**context)
+
+                module._downloads.put((src, dst))
+                module._downloading[dst] = True
+                self.echo("Preparing to download '%s'.." % src)
+
+        offline.setToolTip("Download in progress..")
+        offline.setEnabled(False)
 
     def keyPressEvent(self, event):
         """Delegate keyboard events"""
@@ -465,18 +720,13 @@ QPushButton {
             version_document = version_item.data(DocumentRole)
             self.data["state"]["context"]["version"] = version_document["name"]
 
-            # NOTE(marcus): This is backwards compatible with assets published
-            # before locations were implemented. Newly published assets are
-            # embedded with this attribute, but current ones were not.
-            locations = version_document.get("locations", [])
-
-            self.data["state"]["locations"][:] = locations
-
             representations_by_name = {
                 representation["name"]: [representation]
                 for representation in io.find(
                     {"type": "representation",
                      "parent": version_document["_id"]})
+
+                # Backwards compatibility
                 if representation["name"] not in ("json", "source")
             }
 
@@ -504,11 +754,10 @@ QPushButton {
             # supervisor can configure.
             item = QtWidgets.QListWidgetItem({
                 "ma": "Maya Ascii",
-                "source": "Original Source File",
-                "abc": "Pointcache",
+                "source": "Original source file",
+                "abc": "Alembic",
                 "history": "History",
-                "curves": "Animation Curves",
-                "group": "Asset Group",
+                "curves": "Animation curves",
             }.get(name, name))  # Default to using name as-is
 
             item.setData(QtCore.Qt.ItemIsEnabled, True)
@@ -526,6 +775,7 @@ QPushButton {
         self.echo("Duration: %.3fs" % (time.time() - t1))
 
     def _representationschanged(self):
+        offline = self.data["button"]["offline"]
         load_button = self.data["button"]["load"]
         load_button.setEnabled(False)
 
@@ -546,10 +796,19 @@ QPushButton {
         context = self.data["state"]["context"]
         path = template.format(**context)
 
-        if os.path.exists(path):
+        offline.setEnabled(False)
+
+        if path in module._downloading or os.path.exists(path):
+            offline.setValue(1)
             load_button.setEnabled(True)
+            offline.setToolTip("Already available offline.")
+
         else:
-            load_button.setToolTip("%s not found." % path)
+            offline.setValue(0)
+
+            # Is the representation available at any location?
+            offline.setEnabled(True)
+            offline.setToolTip("Toggle to make available offline.")
 
     def on_refresh_pressed(self):
         self.refresh()
@@ -560,6 +819,8 @@ QPushButton {
         versions_model = models["versions"]
         version_item = versions_model.currentItem()
         version = version_item.data(DocumentRole)
+
+        assert version is not None, "This is a bug"
 
         representations_model = models["representations"]
         representation_item = representations_model.currentItem()
@@ -619,13 +880,8 @@ QPushButton {
         lib.schedule(widget.hide, 5000, channel="message")
 
     def closeEvent(self, event):
-        # Kill on holding SHIFT
-        modifiers = QtWidgets.QApplication.queryKeyboardModifiers()
-        shift_pressed = QtCore.Qt.ShiftModifier & modifiers
-
-        if shift_pressed:
-            print("Force quitted..")
-            self.setAttribute(QtCore.Qt.WA_DeleteOnClose)
+        # Kill download manager
+        module._downloads.put((None, None))
 
         # Kill on holding SHIFT
         modifiers = QtWidgets.QApplication.queryKeyboardModifiers()
@@ -671,7 +927,6 @@ def show(root=None, debug=False, parent=None):
         )
 
         api.Session["AVALON_PROJECT"] = any_project["name"]
-        module.project = any_project["name"]
 
     with lib.application():
         window = Window(parent)
