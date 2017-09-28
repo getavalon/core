9e7aff4ee1a106d0940448ea6647f9ba9cd09929
aardschok <wikoreman@gmail.com>
2017-09-28 10:56:01 +0200

    created install function to call fix specific functions

--- avalon/maya/compat.py
+++ avalon/maya/compat.py
@@ -1,28 +1,29 @@
-"""Compatibility issues six.py / Maya2018
+"""Compatibility
 
-The Maya 2018 version tries to import the `http` module from
-Maya2018\plug-ins\MASH\scripts\googleapiclient\http.py in stead of the module
-from six.py. This import conflict causes a crash Avalon's publisher.
-This is due to Autodesk adding paths to the PYTHONPATH environment variable
-which contain modules instead of only packages.
-
-This will be used until Autodesk has fixed the issue
+This module is
 """
+import os
 
 
-def check_compatibility():
+def remove_googleapiclient():
     """Check if the compatibility must be maintained
 
-    The compatibility will only be maintained when the Maya version is 2018
+    The Maya 2018 version tries to import the `http` module from
+    Maya2018\plug-ins\MASH\scripts\googleapiclient\http.py in stead of the module
+    from six.py. This import conflict causes a crash Avalon's publisher.
+    This is due to Autodesk adding paths to the PYTHONPATH environment variable
+    which contain modules instead of only packages.
     """
 
-    import os
-    import maya.cmds as cmds
+    keyword = "googleapiclient"
 
-    if cmds.about(verison=True) == "2018":
-        keyword = "googleapiclient"
+    # reconstruct python paths
+    python_paths = os.environ["PYTHONPATH"].split(os.pathsep)
+    paths = [path for path in python_paths if keyword not in path]
+    os.environ["PYTHONPATH"] = os.pathsep.join(paths)
 
-        # reconstruct python paths
-        python_paths = os.environ["PYTHONPATH"].split(os.pathsep)
-        paths = [path for path in python_paths if keyword not in path]
-        os.environ["PYTHONPATH"] = os.pathsep.join(paths)
+
+def install():
+    remove_googleapiclient()
+
+
