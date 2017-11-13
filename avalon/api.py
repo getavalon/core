08ecb3110621eadc2b3366191f746791efd5e2af
Roy Nieterau <roy_nieterau@hotmail.com>
2017-11-13 20:44:58 +0100

    Expose `update_current_context` in api

--- avalon/api.py
+++ avalon/api.py
@@ -34,6 +34,7 @@ from .pipeline import (
     update,
     remove,
 
+    update_current_context,
     get_representation_path,
     loaders_from_representation,
 
@@ -79,6 +80,7 @@ __all__ = [
     "update",
     "remove",
 
+    "update_current_context",
     "get_representation_path",
     "loaders_from_representation",
 
