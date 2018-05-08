309e4962bacf2552c1873c42217cb10e91ed2cb5
wijnand <wikoreman@gmail.com>
2018-05-08 15:52:31 +0200

    Refactored out get_family_filters, is replaced by get_data("family_states")

--- avalon/tools/cbloader/lib.py
+++ avalon/tools/cbloader/lib.py
@@ -1,5 +1,3 @@
-import importlib
-
 from ...vendor import qtawesome
 from ... import io, pipeline
 
@@ -37,8 +35,10 @@ def refresh_family_config():
     families = project['config'].get("families", [])
     families = {family['name']: family for family in families}
 
-    # Get all family states based on the config
-    family_states = get_family_filters(families)
+    # Check if any family state are being overwritten by the configuration
+    data = pipeline.get_data("family_states") or {}
+    family_states = {family: data.get(family, True) for family in families}
+
     # Replace icons with a Qt icon we can use in the user interfaces
     default_icon = qtawesome.icon("fa.folder", color=FAMILY_ICON_COLOR)
     for name, family in families.items():
@@ -60,42 +60,3 @@ def refresh_family_config():
     FAMILY_CONFIG.update(families)
 
     return families
-
-
-def get_family_filters(families):
-    """Set the family states based on host settings
-
-    This function has no effect if the lib module or the function in not present
-    in the registered config.
-
-    Args:
-        families (iterable): collection of family names, example:
-                             ["polly.imagesequence", "polly.camera_abc"]
-
-    Returns:
-        dict: collection of family settings
-    """
-
-    # Find host's lib module in the registered config
-    host = pipeline.registered_host()
-    host_name = host.__name__.rsplit(".", 1)[-1]
-
-    config = pipeline.registered_config()
-    config_name = config.__name__.split(".", 1)[0]
-    config_lib_module = "{}.{}.lib".format(config_name, host_name)
-
-    # Check if host is compatible
-    try:
-        lib = importlib.import_module(config_lib_module)
-    except (ValueError, TypeError, RuntimeError):
-        print("Configuration has no module called {0} or "
-              "{0}.lib".format(host_name))
-        return {}
-
-    if not hasattr(lib, "set_family_filter"):
-        return {}
-
-    new_states = {name: {"state": lib.set_family_filter(name)}
-                  for name in families}
-
-    return new_states
