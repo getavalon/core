2256d2c4f55ec8846a975d45cbdf63d52a2d4159
Jakub Trllo <jakub.trllo@gmail.com>
2019-01-31 14:33:06 +0100

    let cleared tasks if nothing found

--- avalon/tools/projectmanager/model.py
+++ avalon/tools/projectmanager/model.py
@@ -237,6 +237,10 @@ class TasksModel(TreeModel):
         #     )
 
         self.clear()
+        # let cleared task view if no tasks are available
+        if len(tasks) == 0:
+            return
+
         self.beginResetModel()
 
         default_icon = self._icons["__default__"]
