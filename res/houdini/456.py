67386d8fa9366e59061b374223edad23a6fa6974
Desmond <tws0002@gmail.com>
2019-04-02 14:31:40 +0800

    Update 456.py

--- res/houdini/456.py
+++ res/houdini/456.py
@@ -1,10 +1,6 @@
-123
-
 import hou
 
 HOU_ACCESS = 2
 
-# Set Environment
-# -----------------------------------------------------------------------------
-hou.hscript('set -g HOUDINI_ACCESS_METHOD = {0}'.format(HOU_ACCESS ))
+hou.hscript('set -g HOUDINI_ACCESS_METHOD = {0}'.format(HOU_ACCESS))
 print('Environment variables set.')
