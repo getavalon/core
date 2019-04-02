b9254e6ea593089ccf4fa3031cd5a9886fd0a68b
Desmond <tws0002@gmail.com>
2019-04-02 13:57:55 +0800

    update 456

--- res/houdini/456.py
+++ res/houdini/456.py
@@ -1,13 +1,10 @@
+123
+
 import hou
 
 HOU_ACCESS = 2
 
 # Set Environment
 # -----------------------------------------------------------------------------
-
-try:
-    hou.hscript('set -g HOUDINI_ACCESS_METHOD = {0}'.format(HOU_ACCESS ))
-    print('Environment variables set.')
-
-# Success messages
-print("456.cmd successfully executed.")
+hou.hscript('set -g HOUDINI_ACCESS_METHOD = {0}'.format(HOU_ACCESS ))
+print('Environment variables set.')
