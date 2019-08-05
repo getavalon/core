999940e76ab3a93d8f60c891504c57fea733e00c
iLLiCiTiT <jakub.trllo@gmail.com>
2019-08-05 19:50:32 +0200

    created _views subfolder in tools that contains all views from tools

---
+++ avalon/tools/_views/__init__.py
@@ -0,0 +1,16 @@
+from ...vendor import Qt
+from Qt import QtCore, QtWidgets
+
+
+from .view_tree_deselectable import DeselectableTreeView
+from .view_asset import AssetView
+
+
+__all__ = [
+    "Qt",
+    "QtCore",
+    "QtWidgets",
+
+    "DeselectableTreeView",
+    "AssetView"
+]
