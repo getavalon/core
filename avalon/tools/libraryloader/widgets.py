e23c16b3cb345bd465cd6798d5bbf80fea130751
iLLiCiTiT <jakub.trllo@gmail.com>
2019-09-23 15:12:06 +0200

    updated library loader widgets

--- avalon/tools/libraryloader/widgets.py
+++ avalon/tools/libraryloader/widgets.py
@@ -8,11 +8,11 @@ from ...vendor import qtawesome
 from ...vendor.Qt import QtWidgets, QtCore
 from ... import style, api, pipeline
 from . import lib
-from .. import lib as toolslib
+from .. import lib as tools_lib
 
 from .models import AssetModel, SubsetsModel
 from ..models import RecursiveSortFilterProxyModel
-from ..loader.model import FamiliesFilterProxyModel
+from ..loader.model import FamiliesFilterProxyModel, SubsetFilterProxyModel
 
 from .delegates import VersionDelegate
 from ..loader.delegates import PrettyTimeDelegate
@@ -26,7 +26,7 @@ class SubsetWidget(QtWidgets.QWidget):
     active_changed = QtCore.Signal()    # active index changed
     version_changed = QtCore.Signal()   # version state changed for a subset
 
-    def __init__(self, dbcon, parent=None):
+    def __init__(self, dbcon, enable_grouping=True, parent=None):
         super(SubsetWidget, self).__init__(parent=parent)
 
         self.dbcon = dbcon
@@ -34,16 +34,25 @@ class SubsetWidget(QtWidgets.QWidget):
         if hasattr(parent, 'tool_name'):
             self.tool_name = parent.tool_name
 
-        model = SubsetsModel(dbcon=self.dbcon, parent=self)
-        proxy = QtCore.QSortFilterProxyModel()
+        model = SubsetsModel(
+            dbcon=self.dbcon, grouping=enable_grouping, parent=self
+        )
+        proxy = SubsetFilterProxyModel()
         family_proxy = FamiliesFilterProxyModel()
         family_proxy.setSourceModel(proxy)
 
         filter = QtWidgets.QLineEdit()
         filter.setPlaceholderText("Filter subsets..")
 
+        groupable = QtWidgets.QCheckBox("Enable Grouping")
+        groupable.setChecked(enable_grouping)
+
+        top_bar_layout = QtWidgets.QHBoxLayout()
+        top_bar_layout.addWidget(filter)
+        top_bar_layout.addWidget(groupable)
+
         view = QtWidgets.QTreeView()
-        view.setIndentation(5)
+        view.setIndentation(20)
         view.setStyleSheet("""
             QTreeView::item{
                 padding: 5px 1px;
@@ -63,7 +72,7 @@ class SubsetWidget(QtWidgets.QWidget):
 
         layout = QtWidgets.QVBoxLayout(self)
         layout.setContentsMargins(0, 0, 0, 0)
-        layout.addWidget(filter)
+        layout.addLayout(top_bar_layout)
         layout.addWidget(view)
 
         view.setContextMenuPolicy(QtCore.Qt.CustomContextMenu)
@@ -76,6 +85,9 @@ class SubsetWidget(QtWidgets.QWidget):
             "delegates": {
                 "version": version_delegate,
                 "time": time_delegate
+            },
+            "state": {
+                "groupable": groupable
             }
         }
 
@@ -93,22 +105,44 @@ class SubsetWidget(QtWidgets.QWidget):
         self.view.setModel(self.family_proxy)
         self.view.customContextMenuRequested.connect(self.on_context_menu)
 
+        header = self.view.header()
+        # Enforce the columns to fit the data (purely cosmetic)
+        if Qt.__binding__ in ("PySide2", "PyQt5"):
+            header.setSectionResizeMode(QtWidgets.QHeaderView.ResizeToContents)
+        else:
+            header.setResizeMode(QtWidgets.QHeaderView.ResizeToContents)
+
         selection = view.selectionModel()
         selection.selectionChanged.connect(self.active_changed)
 
         version_delegate.version_changed.connect(self.version_changed)
 
+        groupable.stateChanged.connect(self.set_grouping)
+
         self.filter.textChanged.connect(self.proxy.setFilterRegExp)
+        self.filter.textChanged.connect(self.view.expandAll)
 
         self.model.refresh()
 
         # Expose this from the widget as a method
         self.set_family_filters = self.family_proxy.setFamiliesFilter
 
+    def is_groupable(self):
+        return self.data["state"]["groupable"].checkState()
+
+    def set_grouping(self, state):
+        with tools_lib.preserve_selection(tree_view=self.view,
+                                          current_index=False):
+            self.model.set_grouping(state)
+
     def on_context_menu(self, point):
 
         point_index = self.view.indexAt(point)
         if not point_index.isValid():
+            return
+
+        item = point_index.data(self.model.ItemRole)
+        if item.get("isGroup"):
             return
 
         # Get all representation->loader combinations available for the
@@ -124,13 +158,10 @@ class SubsetWidget(QtWidgets.QWidget):
                         available_loaders.remove(loader)
 
         loaders = list()
-        item = point_index.data(self.model.ItemRole)
-        version_id = item['version_document']['_id']
 
-        representations = self.dbcon.find({
-            "type": "representation",
-            "parent": version_id}
-        )
+        version_id = item["version_document"]["_id"]
+        representations = self.dbcon.find({"type": "representation",
+                                   "parent": version_id})
         for representation in representations:
             for loader in lib.loaders_from_representation(
                 self.dbcon,
@@ -159,7 +190,7 @@ class SubsetWidget(QtWidgets.QWidget):
                 label = loader.__name__
 
             # Add the representation as suffix
-            label = "{0} ({1})".format(label, representation['name'])
+            label = "{0} ({1})".format(label, representation["name"])
 
             action = QtWidgets.QAction(label, menu)
             action.setData((representation, loader))
@@ -192,7 +223,7 @@ class SubsetWidget(QtWidgets.QWidget):
 
         # Find the representation name and loader to trigger
         action_representation, loader = action.data()
-        representation_name = action_representation['name']  # extension
+        representation_name = action_representation["name"]  # extension
 
         # Run the loader for all selected indices, for those that have the
         # same representation available
@@ -213,6 +244,9 @@ class SubsetWidget(QtWidgets.QWidget):
         # Trigger
         for row in rows:
             item = row.data(self.model.ItemRole)
+            if item.get("isGroup"):
+                continue
+
             version_id = item["version_document"]["_id"]
             representation = self.dbcon.find_one({
                 "type": "representation",
@@ -235,6 +269,39 @@ class SubsetWidget(QtWidgets.QWidget):
             except pipeline.IncompatibleLoaderError as exc:
                 self.echo(exc)
                 continue
+
+    def selected_subsets(self):
+        selection = self.view.selectionModel()
+        rows = selection.selectedRows(column=0)
+
+        subsets = list()
+        for row in rows:
+            item = row.data(self.model.ItemRole)
+            if not item.get("isGroup"):
+                subsets.append(item)
+
+        return subsets
+
+    def group_subsets(self, name, asset_id, items):
+        field = "data.subsetGroup"
+
+        if name:
+            update = {"$set": {field: name}}
+            self.echo("Group subsets to '%s'.." % name)
+        else:
+            update = {"$unset": {field: ""}}
+            self.echo("Ungroup subsets..")
+
+        subsets = list()
+        for item in items:
+            subsets.append(item["subset"])
+
+        filter = {
+            "type": "subset",
+            "parent": asset_id,
+            "name": {"$in": subsets},
+        }
+        self.dbcon.update_many(filter, update)
 
     def echo(self, message):
         print(message)
@@ -419,7 +486,10 @@ class FamilyListWidget(QtWidgets.QListWidget):
         self.clear()
         for name in sorted(unique_families):
 
-            family = lib.get(lib.FAMILY_CONFIG, name)
+            family = lib.get_family_cached_config(name)
+            if family.get("hideFilter"):
+                continue
+
             label = family.get("label", name)
             icon = family.get("icon", None)
 
@@ -619,7 +689,7 @@ class AssetWidget(QtWidgets.QWidget):
 
         # Select
         mode = selection_model.Select | selection_model.Rows
-        for index in toolslib.iter_model_rows(self.proxy,
+        for index in tools_lib.iter_model_rows(self.proxy,
                                       column=0,
                                       include_root=False):
             data = index.data(self.model.ItemRole)
