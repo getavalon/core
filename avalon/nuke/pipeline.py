e836087d0e0ee7fcb9026a8c5e72c87cef4b6955
Jakub Jezek <jakub@orbi.tools>
2019-11-21 17:15:32 +0100

    feat(nuke): adding subset node duplicity check to Creator

--- avalon/nuke/pipeline.py
+++ avalon/nuke/pipeline.py
@@ -165,6 +165,18 @@ class Creator(api.Creator):
     """
     node_color = "0xdfea5dff"
 
+    def __init__(self, *args, **kwargs):
+        super(Creator, self).__init__(*args, **kwargs)
+        # make sure no duplicity of subsets are in workfile
+        if lib.check_subsetname_exists(
+                nuke.allNodes(),
+                self.data["subset"]):
+            msg = "The subset name `{0}` is already used on a node in" \
+                  "this workfile.".format(self.data["subset"])
+            self.log.error(msg + '\n\nPlease use other subset name!')
+            raise NameError("`{0}: {1}".format(__name__, msg))
+        return
+
     def process(self):
         from nukescripts import autoBackdrop
         nodes = list()
