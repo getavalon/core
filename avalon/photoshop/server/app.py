4df1b7e7783b34d8e6e8725b7bb708ae556d206c
Toke Jepsen <tokejepsen@gmail.com>
2020-01-27 14:20:09 +0000

    Revert using separate process for launching tools.
    - Also add project manager.

--- avalon/photoshop/server/app.py
+++ avalon/photoshop/server/app.py
@@ -1,11 +1,19 @@
 import os
-import subprocess
 
-from avalon import api, io
-from avalon.tools import publish
-import pyblish.api
-import avalon.photoshop
+from avalon import api, photoshop
+from avalon.tools import (
+    contextmanager,
+    workfiles,
+    creator,
+    loader,
+    publish,
+    sceneinventory,
+    projectmanager
+)
 from avalon.vendor.bottle import route, template, run
+
+
+api.install(photoshop)
 
 
 @route("/")
@@ -13,24 +21,9 @@ def index():
     return template(os.path.join(os.path.dirname(__file__), "index.html"))
 
 
-def _show(module_name):
-    cmd = """
-from avalon import api, io
-from avalon.tools import {0}
-import avalon.photoshop
-io.install()
-api.register_host(avalon.photoshop)
-{0}.show()
-"""
-
-    # Need to start a separate process in order for the window to appear ontop
-    # of Photoshop.
-    subprocess.Popen(["python", "-c", cmd.format(module_name)])
-
-
 @route("/context_route")
 def context_route():
-    _show("contextmanager")
+    contextmanager.show()
 
     # Required return statement.
     return "nothing"
@@ -38,7 +31,7 @@ def context_route():
 
 @route("/workfiles_route")
 def workfiles_route():
-    _show("workfiles")
+    workfiles.show()
 
     # Required return statement.
     return "nothing"
@@ -46,7 +39,7 @@ def workfiles_route():
 
 @route("/creator_route")
 def creator_route():
-    _show("creator")
+    creator.show()
 
     # Required return statement.
     return "nothing"
@@ -54,7 +47,7 @@ def creator_route():
 
 @route("/loader_route")
 def loader_route():
-    _show("loader")
+    loader.show()
 
     # Required return statement.
     return "nothing"
@@ -62,16 +55,7 @@ def loader_route():
 
 @route("/publish_route")
 def publish_route():
-
-    guis = pyblish.api.registered_guis()
-    if guis and guis[0] == "pyblish_lite":
-        _show("publish")
-    else:
-        # Pyblish-QML appears ontop of Photoshop, so we dont need to subprocess
-        # it.
-        io.install()
-        api.register_host(avalon.photoshop)
-        publish.show()
+    publish.show()
 
     # Required return statement.
     return "nothing"
@@ -79,11 +63,23 @@ def publish_route():
 
 @route("/manage_route")
 def manage_route():
-    _show("sceneinventory")
+    sceneinventory.show()
 
     # Required return statement.
     return "nothing"
 
 
-if __name__ == "__main__":
+@route("/project_manager_route")
+def project_manager_route():
+    projectmanager.show()
+
+    # Required return statement.
+    return "nothing"
+
+
+def start_server():
     run(host="localhost", port=5000)
+
+
+if __name__ == "__main__":
+    start_server()
