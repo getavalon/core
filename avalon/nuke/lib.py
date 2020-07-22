73747459fb5805e3a65cd14fe21b4a8cd0cc85f7
David Lai <davidlatwe@gmail.com>
2020-07-22 18:23:59 +0800

    Fix knob KeyError when syncing in multi-view session

--- avalon/nuke/lib.py
+++ avalon/nuke/lib.py
@@ -228,12 +228,20 @@ def sync_copies(nodes, force=False):
         origin[node] = sources
 
         for copy in find_copies(node):
+            copy_name = copy.fullName()
+
             for name, knob in copy.knobs().items():
                 if name not in sources:
                     continue
                 # Only update knob that hasn't been modified
                 if force or is_knob_eq(sources[name], knob):
-                    targets.append(knob.fullyQualifiedName())
+                    # (NOTE) If current Nuke session has multiple views, like
+                    #   working on stereo shot, `knob.fullyQualifiedName` will
+                    #   append current view at the end of the knob name, for
+                    #   example "renderlayer.Read1.first.left".
+                    #   Assemble from node's full name and knob name to avoid
+                    #   that.
+                    targets.append("%s.%s" % (copy_name, knob.name()))
 
         if targets:
             staged[node] = targets
