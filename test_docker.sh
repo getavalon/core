60933104664ecc46767e5c70534eb3a27756401f
David Lai <davidlatwe@gmail.com>
2020-07-27 17:26:53 +0800

    Fix coverage not being submitted

--- test_docker.sh
+++ test_docker.sh
@@ -6,6 +6,9 @@ docker run \
   --link avalon-mongo:mongo \
   --env AVALON_SILENT \
   --env AVALON_MONGO=mongodb://mongo:27017 \
+  --env TRAVIS_JOB_ID \
+  --env TRAVIS_BRANCH \
+  --env COVERALLS_REPO_TOKEN \
   avalon/core:maya
 # Rename coverage data file and will be combined later
 mv .coverage .coverage.maya
